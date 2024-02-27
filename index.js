72eb3b925c385fb660bccaf11fce29f12e7843cb
shinhyojeong <shinhyojeong.w@gmail.com>
2024-02-28 00:50:24 +0900

    feat: lint 적용 안되는 이슈 수정

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 module.exports = {
   root: true,
   parserOptions: {
-    sourceType: "module",
-    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaVersion: 2018
   },
   env: {
     es2021: true,
     browser: true,
-    node: true,
+    node: true
   },
-  extends: ["./src/import", "./src/base", "plugin:prettier/recommended"],
+  extends: ['./src/import', './src/base', 'plugin:prettier/recommended'],
   overrides: [
     {
-      files: ["**/*.ts", "**/*.tsx"],
-      parser: "@typescript-eslint/parser",
-      extends: ["plugin:@typescript-eslint/recommended", "./src/typescript"],
-      plugins: ["@typescript-eslint"],
+      files: ['**/*.ts', '**/*.tsx'],
+      parser: '@typescript-eslint/parser',
+      extends: ['plugin:@typescript-eslint/recommended', './src/typescript'],
+      plugins: ['@typescript-eslint'],
       settings: {
-        "import/resolver": {
-          typescript: {},
-        },
-      },
-    },
-  ],
-};
+        'import/resolver': {
+          typescript: {}
+        }
+      }
+    }
+  ]
+}
