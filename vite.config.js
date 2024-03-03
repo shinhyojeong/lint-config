53379f849e48a5cb464d372cc73aa3a70d8a759d
shinhyojeong <shinhyojeong.w@gmail.com>
2024-03-04 00:56:36 +0900

    config: build 설정 수정, export 설정 수정

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,16 @@
-import { resolve } from "path";
-import { defineConfig } from "vite";
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve(__dirname, "index.js"),
-      name: "shinhyojeong-eslint-config",
-      fileName: "index",
-      formats: ["es", "cjs"],
+      entry: [
+        resolve(__dirname, 'index.js'),
+        resolve(__dirname, 'prettier.js')
+      ],
+      name: '@shinhyojeong/lint-config',
+      formats: ['es', 'cjs']
     },
-    outDir: "lib",
-  },
-});
+    outDir: 'lib'
+  }
+})
