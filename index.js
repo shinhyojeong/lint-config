29a1d227e31d60c2852abf8e2f19d9607a2bc6d3
shinhyojeong <shinhyojeong.w@gmail.com>
2024-03-06 02:46:58 +0900

    fix: add src to pakcage.json

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const auto = require('./src/auto')
-const base = require('./src/base')
-const react = require('./src/react')
-const prettier = require('./src/prettier')
-const typescript = require('./src/typescript')
+const auto = require('../src/auto')
+const base = require('../src/base')
+const react = require('../src/react')
+const prettier = require('../src/prettier')
+const typescript = require('../src/typescript')
 
 module.exports = {
   configs: {
