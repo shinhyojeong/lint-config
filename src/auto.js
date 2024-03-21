87923b74451fc2bc6ee49073601fa0687bfede5c
shinhyojeong <shinhyojeong.w@gmail.com>
2024-03-21 20:48:00 +0900

    feat: 경로 resolver

--- src/auto.js
+++ src/auto.js
@@ -9,12 +9,15 @@ module.exports = {
     browser: true,
     node: true
   },
-  extends: ['./import', './base'],
+  extends: ['./import', './base'].map(require.resolve),
   overrides: [
     {
       files: ['**/*.ts', '**/*.tsx'],
       parser: '@typescript-eslint/parser',
-      extends: ['plugin:@typescript-eslint/recommended', './typescript'],
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        ...['./typescript'].map(require.resolve)
+      ],
       plugins: ['@typescript-eslint'],
       settings: {
         'import/resolver': {
